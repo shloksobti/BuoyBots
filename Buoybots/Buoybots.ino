4fc887a2971e36da763d9faeeeb76826e1f38f4b
Shlok Sobti <shlok.sobti@gmail.com>
2019-02-06 21:19:43 -0600

    Updates

--- Buoybots/Buoybots.ino
+++ Buoybots/Buoybots.ino
@@ -1,4 +1,4 @@
-#include "Kalman.h"
+ #include "Kalman.h"
 #include "Control.h"
 
 float measuredValue1;
@@ -15,7 +15,7 @@ Kalman myfilter(0.125,32,1023,0); // double process_noise, double sensor_noise, 
 Control myControl();
 
 float voltageToDepth(float voltage){
-  float depth = 42.26*voltage - 69.3 -7.35;
+  float depth = (231/52)*voltage - (231/40);
   return depth;
 }	
 void setup() {
@@ -38,12 +38,6 @@ void loop() {
    measuredVoltage = measuredValue1 * 5/1023;
    filteredVoltage = filteredValue1 * 5/1023;
    depth = voltageToDepth(filteredVoltage);
-//   
-//   measuredValue2 = (double) analogRead(A2);
-//   filteredValue2 = myfilter.getFilteredValue(measuredValue2);
-//
-//   depth = myControl.getDepth(filteredValue1, filteredValue2);
-//   attitude = myControl.getAttitude(filteredValue1, filteredValue2);
 //   
    Serial.println(depth,6);
 
